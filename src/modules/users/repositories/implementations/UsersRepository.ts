002869ba81f3bde518d0f4bf78d51a4ab2eb34bb
Ewerson Vieira <ewersonv@mobilesaude.com.br>
2022-03-26 13:23:08 -0300

    adicionando resolução do desafio

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,17 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    return this.repository.findOneOrFail(user_id, { relations: ["games"] });
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY users.first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users WHERE users.first_name ILIKE $1 AND users.last_name ILIKE $2", [first_name, last_name]); // Complete usando raw query
   }
 }
